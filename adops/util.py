104688532bcefba697a605eb0adc98ee39d36a6a
3neat <br@3neat.com>
2015-05-27 22:58:54 -0700

    Refactored dataframe to csv export process into create_report()

--- adops/util.py
+++ adops/util.py
@@ -1,6 +1,25 @@
 import pandas as pd
+import numpy as np
 from os import path
+from adops import report
 
+
+# STATIC columns, new names
+SITE_COLUMNS = ['Advertiser Id', 'Campaign Id', 'Ad Group Id', 'Site', 'Supply Vendor', 'Advertiser Name',
+                          'Campaign Name', 'Ad Group Name', 'Bids', 'Bid Amount', 'Imps', 'Clicks', 'PC 1', 'PC 2',
+                          'PC 3', 'PC 4', 'PC 5', 'PI 1', 'PI 2', 'PI 3', 'PI 4', 'PI 5', 'Advertiser Total Cost',
+                          'CreativeIsTrackable', 'CreativeWasViewable']
+
+SITE_NAME = ['advertiser_id', 'campaign_id', 'adgroup_id', 'site', 'supply_vendor', 'advertiser_name',
+                        'campaign_name', 'adgroup_name', 'bids', 'bid_amount', 'impressions', 'clicks',
+                        'ctc_1', 'ctc_2', 'ctc_3', 'ctc_4', 'ctc_5', 'vtc_1','vtc_2', 'vtc_3', 'vtc_4', 'vtc_5',
+                        'cost', 'creative_is_trackable', 'creative_was_viewable']
+
+SITE_NEW_NAME = ['advertiser_id', 'campaign_id', 'adgroup_id', 'site', 'supply_vendor', 'advertiser_name',
+                        'campaign_name', 'adgroup_name', 'bids', 'bid_amount', 'impressions', 'clicks',
+                        'ctc_1', 'ctc_2', 'ctc_3', 'ctc_4', 'ctc_5', 'vtc_1','vtc_2', 'vtc_3', 'vtc_4', 'vtc_5',
+                        'cost', 'creative_is_trackable', 'creative_was_viewable','ctr', 'win_rate', 'avg_bid', 'tc',
+                        'ecpc', 'ecpm', 'ecpa', 'moat']
 
 def combine_reports(files, columns, folder):
     # Return an aggregate DataFrame for a given set of files
@@ -17,7 +36,7 @@ def combine_reports(files, columns, folder):
 
 
 def add_metrics(df):
-
+    # Add common metrics to DataFrame
     df['ctr'] = df.clicks / df.impressions
     df['win_rate'] = df.impressions / df.bids
     df['avg_bid'] = df.bid_amount / df.bids * 1000
@@ -27,3 +46,19 @@ def add_metrics(df):
     df['ecpa'] = df.cost / df.tc
     df['moat'] = df.creative_was_viewable / df.creative_is_trackable
     return df
+
+
+def create_report(folder, reports, view):
+    # Filter out the needed report files for analysis
+    filtered_reports = report.report_filter(reports, **view)
+
+    # Transform report files to working DataFrame
+    df = combine_reports(filtered_reports, SITE_COLUMNS, folder)
+    df.columns = SITE_NAME
+
+    df = df.groupby('site')
+    df = df[[x for x in SITE_NAME]].aggregate(np.sum)
+
+    df = add_metrics(df)
+
+    df.to_csv(''.join([view['name'], '.csv']))
--- main.py
+++ main.py
@@ -1,23 +1,5 @@
 import os
-import numpy as np
 from adops import report, util
-
-# STATIC columns, new names
-SITE_COLUMNS = ['Advertiser Id', 'Campaign Id', 'Ad Group Id', 'Site', 'Supply Vendor', 'Advertiser Name',
-                          'Campaign Name', 'Ad Group Name', 'Bids', 'Bid Amount', 'Imps', 'Clicks', 'PC 1', 'PC 2',
-                          'PC 3', 'PC 4', 'PC 5', 'PI 1', 'PI 2', 'PI 3', 'PI 4', 'PI 5', 'Advertiser Total Cost',
-                          'CreativeIsTrackable', 'CreativeWasViewable']
-
-SITE_NAME = ['advertiser_id', 'campaign_id', 'adgroup_id', 'site', 'supply_vendor', 'advertiser_name',
-                        'campaign_name', 'adgroup_name', 'bids', 'bid_amount', 'impressions', 'clicks',
-                        'ctc_1', 'ctc_2', 'ctc_3', 'ctc_4', 'ctc_5', 'vtc_1','vtc_2', 'vtc_3', 'vtc_4', 'vtc_5',
-                        'cost', 'creative_is_trackable', 'creative_was_viewable']
-
-SITE_NEW_NAME = ['advertiser_id', 'campaign_id', 'adgroup_id', 'site', 'supply_vendor', 'advertiser_name',
-                        'campaign_name', 'adgroup_name', 'bids', 'bid_amount', 'impressions', 'clicks',
-                        'ctc_1', 'ctc_2', 'ctc_3', 'ctc_4', 'ctc_5', 'vtc_1','vtc_2', 'vtc_3', 'vtc_4', 'vtc_5',
-                        'cost', 'creative_is_trackable', 'creative_was_viewable','ctr', 'win_rate', 'avg_bid', 'tc',
-                        'ecpc', 'ecpm', 'ecpa', 'moat']
 
 
 def init_reports(folder):
@@ -27,8 +9,6 @@ def init_reports(folder):
         if f.endswith(".tsv"):
             rpt.append(report.Report(f))
     return rpt
-
-
 
 current_directory = os.getcwd()
 folder = os.path.join(current_directory, 'reports/')
@@ -40,20 +20,11 @@ view = {
 	'advertiser': ['xbci0tw', 'nk6bz6j'],
 	'campaign': [],
 	'adgroup': [],
-    'date_range': None
+    'date_range': None,
+    'name': 'ChannelLock-FranNet_SiteAnalysis'
 }
 
-# Filter out the needed report files for analysis
-filtered_reports = report.report_filter(reports, **view)
-
-# Transform report files to working DataFrame
-df = util.combine_reports(filtered_reports, SITE_COLUMNS, folder)
-df.columns = SITE_NAME
-
-df = df.groupby('site')
-df = df[[x for x in SITE_NAME]].aggregate(np.sum)
-
-df = util.add_metrics(df)
+util.create_report(folder, reports, view)
 
 # NOTES:
 # * All of the above functions will produce an advertiser / filtered Site Report data frame
@@ -61,5 +32,3 @@ df = util.add_metrics(df)
 # TODO: 0 - Create these data frames based on saved "rule/group" structure
 # TODO: 0 - After a rule structure, need to think of UI
 # TODO: 1 - Refactor below to __main__
-for i,x in enumerate(filtered_reports):
-    print str(i) + ": " + x.filepath

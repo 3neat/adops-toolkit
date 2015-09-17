21b78c6f0060d13e8839c83844d568ca325de621
3neat <br@3neat.com>
2015-09-17 10:44:14 -0700

    added ability to read in local ENV var for settings filename

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 import yaml
+import os
 
-with open("settings.yaml", "r") as f:
+# This allows us to define an env variable to set local settings to wherever adops toolkit is deployed
+
+# Set default
+if os.getenv("ADOPSSETTINGS") is None:
+    os.environ["ADOPSSETTINGS"] = "settings.yaml"
+
+settings_file = os.environ["ADOPSSETTINGS"]
+
+with open(settings_file, "r") as f:
     settings = yaml.load(f)
